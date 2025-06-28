212d480ba2c7e2ebf971f41a640067f5c8c7f5db
hp <ahmedshehta0123@gmail.com>
2025-06-28 15:19:56 +0300

    add template and add test

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase,SimpleTestCase
+from django.urls import reverse, resolve
+
+from pages.views import HomePageView
+
 
 # Create your tests here.
+class HomePageTest(SimpleTestCase):
+    def setUp(self):
+        url = reverse("home")
+        self.response = self.client.get(url)
+
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, "home.html")
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, "home page")
+
+    def test_homepage_url_resolves_homepage_view(self):  # new
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
